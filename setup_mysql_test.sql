2376c8377f94d75700d4bce7e953fd56f7b7703d
UmarFarukA <talk2ufaz@gmail.com>
2023-03-20 05:58:56 -0700

    Initial commit

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Create hbnb_test_db database and hbnb_test user
+CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON 'hbnb_test_db' TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON `perfomance_schema`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
