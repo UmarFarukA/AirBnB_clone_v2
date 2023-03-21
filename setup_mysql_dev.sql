6bba72cd97939d9653ae905e4db9d11944c2e802
UmarFarukA <talk2ufaz@gmail.com>
2023-03-21 03:20:09 -0700

    remove:spaces

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,14 +2,14 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+   IF NOT EXISTS 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES
-   ON hbnb_dev_db.*
+   ON `hbnb_dev_db`.*
    TO 'hbnb_dev'@'localhost'
    IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT
-   ON performance_schema.*
+   ON `performance_schema`.*
    TO 'hbnb_dev'@'localhost'
    IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,14 @@
 -- Create hbnb_test_db database and hbnb_test user
 CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
+   IF NOT EXISTS 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
-    ON 'hbnb_test_db'
-    TO 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
+   ON 'hbnb_test_db'
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT
-    ON `performance_schema`.*
-    TO 'hbnb_test'@'localhost'
-    IDENTIFIED BY 'hbnb_test_pwd';
+   ON `performance_schema`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
