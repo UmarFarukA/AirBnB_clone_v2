85d7680a2eb8903a49ba9f5ede4df1543f456df1
UmarFarukA <talk2ufaz@gmail.com>
2023-04-06 08:31:34 -0700

    add:if

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,13 +23,11 @@ def do_deploy(archive_path):
 
     if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
     if run("rm -rf /data/web_static/releases/{}/".
            format(name)).failed is True:
         return False
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
         return False
     if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
            format(file, name)).failed is True:
@@ -41,6 +39,8 @@ def do_deploy(archive_path):
         return False
     if run("rm -rf /data/web_static/releases/{}/web_static".
            format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
         return False
     if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
