6b7eb4962bcd7c866e5b3581d7ec37a4e388d2d3
UmarFarukA <talk2ufaz@gmail.com>
2023-04-06 06:05:55 -0700

    correct:format

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,12 +9,12 @@ from fabric.api import local
 def do_pack():
     """Create a .tgz archive for web static content"""
     dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}".format(dt.year,
-                                                     dt.month,
-                                                     dt.day,
-                                                     dt.hour,
-                                                     dt.minute,
-                                                     dt.second)
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").Failed is True:
             return None
