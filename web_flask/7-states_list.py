92990bee00509016aaef5405566083ad27672141
UmarFarukA <talk2ufaz@gmail.com>
2023-04-22 15:08:21 -0700

    initial commit

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A scripts that start python flask App"""
+from flask import Flask
+from flask import render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/state_list', strict_slashes=False)
+def state_list():
+    """Display list of states"""
+    states = storage.all("States")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Removing the current session"""
+    storage.close()
+
+
+if __name__ = "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ web_flask/9-states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""A script that start python flask App"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=True)
+def states():
+    """Function that display all states"""
+    states = storage.all("State")
+    return render_template("9-states.html", states=states)
+
+
+@app.route('/states/<id>', strict_slashes=True)
+def state_by_id(id):
+    """Function that get states by id"""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Function that close current session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
