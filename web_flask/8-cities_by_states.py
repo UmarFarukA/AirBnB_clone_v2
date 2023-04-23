db486e636c98da5c5bef1209b9abfe5487d2e4cf
UmarFarukA <talk2ufaz@gmail.com>
2023-04-23 08:54:54 -0700

    correct route spellings

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""A scripts that start flask app
-The app listen on 0.0.0.0 on port 5000
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
 Routes:
-    /cities_by_states: List all states & related cities
+    /cities_by_states: HTML page with a list of all states and related cities.
 """
+from models import storage
 from flask import Flask
 from flask import render_template
-from models import storage
-
 
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Function that load all cities to state"""
+    """Displays an HTML page with a list of all states and related cities.
+    States/cities are sorted by name.
+    """
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Function that remove current session"""
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
