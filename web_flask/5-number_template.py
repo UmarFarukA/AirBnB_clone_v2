02308366763e4d1bc78b89106c8c2a6ebe5fddcb
UmarFarukA <talk2ufaz@gmail.com>
2023-04-23 03:29:42 -0700

    adjust qoute

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -14,13 +14,13 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Display Hello HBNB!"""
-    return “Hello HBNB!”
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Display HBNB"""
-    return “HBNB”
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
